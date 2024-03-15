eedd8f7f6b104a214c2e75de840f730d2219d730
lacdev <56662567+lacdev@users.noreply.github.com>
2024-03-14 22:33:26 -0600

    add mocks for tolerance and multipliers on source folder

---
+++ src/mocks/multiplier.ts
@@ -0,0 +1,12 @@
+export const MULTIPLIER_MOCK_DATA = [
+  { color: 'black', multiplier: 1 },
+  { color: 'brown', multiplier: 10 },
+  { color: 'red', multiplier: 100 },
+  { color: 'orange', multiplier: 1000 },
+  { color: 'yellow', multiplier: 10000 },
+  { color: 'green', multiplier: 100000 },
+  { color: 'blue', multiplier: 1000000 },
+  { color: 'violet', multiplier: 10000000 },
+  { color: 'gold', multiplier: 0.1 },
+  { color: 'silver', multiplier: 0.01 },
+];
---
+++ src/mocks/tolerance.ts
@@ -0,0 +1,10 @@
+export const TOLERANCE_MOCK_DATA = [
+  { color: 'brown', tolerance: '+1%' },
+  { color: 'red', tolerance: '+2%' },
+  { color: 'green', tolerance: '+0.5%' },
+  { color: 'blue', tolerance: '+0.25%' },
+  { color: 'violet', tolerance: '+0.1%' },
+  { color: 'grey', tolerance: '+0.05%' },
+  { color: 'gold', tolerance: '+5%' },
+  { color: 'silver', tolerance: '+10%' },
+];
