e23b4021cd0cc2c3a739a0c614f63b1528a62cad
lacdev <56662567+lacdev@users.noreply.github.com>
2024-03-14 22:33:52 -0600

    update mock import on tolerance and multiplier services

--- src/services/multiplier/multiplier.service.ts
+++ src/services/multiplier/multiplier.service.ts
@@ -1,18 +1,8 @@
 import { Injectable } from '@nestjs/common';
+
 import { Multiplier } from '../../entities/multiplier.entity';
 
-const MULTIPLIER_MOCK_DATA = [
-  { color: 'black', multiplier: 1 },
-  { color: 'brown', multiplier: 10 },
-  { color: 'red', multiplier: 100 },
-  { color: 'orange', multiplier: 1000 },
-  { color: 'yellow', multiplier: 10000 },
-  { color: 'green', multiplier: 100000 },
-  { color: 'blue', multiplier: 1000000 },
-  { color: 'violet', multiplier: 10000000 },
-  { color: 'gold', multiplier: 0.1 },
-  { color: 'silver', multiplier: 0.01 },
-];
+import { MULTIPLIER_MOCK_DATA } from '../../mocks/multiplier';
 
 @Injectable()
 export class MultiplierService {
