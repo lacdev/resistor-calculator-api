c230d18f4121184d2874db716430f136aefe4862
lacdev <56662567+lacdev@users.noreply.github.com>
2024-03-18 23:55:11 -0600

    update services tests

--- src/services/multiplier/multiplier.service.spec.ts
+++ src/services/multiplier/multiplier.service.spec.ts
@@ -1,12 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
 import { MultiplierService } from './multiplier.service';
+import { Multiplier } from '../../entities/multiplier.entity';
 
 describe('MultiplierService', () => {
   let service: MultiplierService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MultiplierService],
+      providers: [
+        MultiplierService,
+        { provide: getRepositoryToken(Multiplier), useValue: {} },
+      ],
     }).compile();
 
     service = module.get<MultiplierService>(MultiplierService);
