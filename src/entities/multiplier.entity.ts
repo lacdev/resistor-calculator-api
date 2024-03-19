4085f67d2b550b9c38cbcf701225ef39d8a32bd9
lacdev <56662567+lacdev@users.noreply.github.com>
2024-03-18 23:54:04 -0600

    update tolerance and multilplier entities with TypeORM decorators

--- src/entities/multiplier.entity.ts
+++ src/entities/multiplier.entity.ts
@@ -1,4 +1,12 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Multiplier {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
   color: string;
+  @Column({ type: 'double precision', default: 0 })
   multiplier: number;
 }
--- src/entities/tolerance.entity.ts
+++ src/entities/tolerance.entity.ts
@@ -1,4 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Tolerance {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
   color: string;
+
+  @Column()
   tolerance: string;
 }
