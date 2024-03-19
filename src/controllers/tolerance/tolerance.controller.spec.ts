d25bd78d93c7094d6a891053732081a458244659
lacdev <56662567+lacdev@users.noreply.github.com>
2024-03-18 23:55:28 -0600

    update controllers tests

--- src/controllers/tolerance/tolerance.controller.spec.ts
+++ src/controllers/tolerance/tolerance.controller.spec.ts
@@ -1,19 +1,38 @@
 import { ToleranceController } from './tolerance.controller';
 import { ToleranceService } from '../../services/tolerance/tolerance.service';
 import { TOLERANCE_MOCK_DATA } from '../../mocks/tolerance';
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Tolerance } from '../../entities/tolerance.entity';
+
+const mockToleranceRepository = {
+  find: jest.fn().mockResolvedValue(TOLERANCE_MOCK_DATA),
+};
 
 describe('ToleranceController', () => {
   let toleranceController: ToleranceController;
   let toleranceService: ToleranceService;
 
-  beforeEach(() => {
-    toleranceService = new ToleranceService();
-    toleranceController = new ToleranceController(toleranceService);
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [],
+      providers: [
+        ToleranceService,
+        {
+          provide: getRepositoryToken(Tolerance),
+          useValue: mockToleranceRepository,
+        },
+        ToleranceController,
+      ],
+    }).compile();
+
+    toleranceService = module.get<ToleranceService>(ToleranceService);
+    toleranceController = module.get<ToleranceController>(ToleranceController);
   });
 
   describe('findAll', () => {
     it('should return an array of multiplier values correctly', async () => {
-      const result = TOLERANCE_MOCK_DATA;
+      const result = Promise.resolve(TOLERANCE_MOCK_DATA);
 
       jest.spyOn(toleranceService, 'findAll').mockImplementation(() => result);
 
